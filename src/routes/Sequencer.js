080c7ea91066aafa3f12e7e1a98462eda6370c39
kittybagpipes <sparticus978@gmail.com>
2020-12-20 14:25:13 -0800

    audio buffers set

--- src/routes/Sequencer.js
+++ src/routes/Sequencer.js
@@ -20,8 +20,8 @@ export default class Sequencer extends React.Component {
       saveSuccessful: false,
     };
     //setup sampler
-    this.drumMachine = new Tone.Sampler({
-      urls: {
+    this.drumSamples = new Tone.ToneAudioBuffers(
+      {
         A1: "/sounds/Kick.wav",
         B1: "/sounds/snare.wav",
         C1: "/sounds/shaker.wav",
@@ -29,7 +29,19 @@ export default class Sequencer extends React.Component {
         E1: "/sounds/open-hat.wav",
         F1: "/sounds/shout.wav",
       },
-    }).toDestination();
+      () => {
+        this.drumMachine = new Tone.Sampler({
+          urls: {
+            A1: "/sounds/Kick.wav",
+            B1: "/sounds/snare.wav",
+            C1: "/sounds/shaker.wav",
+            D1: "/sounds/closed-hat.wav",
+            E1: "/sounds/open-hat.wav",
+            F1: "/sounds/shout.wav",
+          },
+        }).toDestination();
+      }
+    );
     Tone.Transport.start();
 
     this.part = new Tone.Part((time, note) => {
