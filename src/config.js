9d9f814fdbdbacb95209b5434c2dcc69cebfd2c7
kittybagpipes <sparticus978@gmail.com>
2020-12-28 10:03:54 -0800

    deploy ready

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   API_TOKEN: process.env.REACT_APP_API_TOKEN,
-  API_BASE_URL: "http://localhost:8000/api",
+  API_BASE_URL: "https://sheltered-reef-55239.herokuapp.com/api",
   TOKEN_KEY: "bap-tracker-token",
 };
