a0ee9fefe5d7479d3507faf3458a938df1d97d6c
kittybagpipes <sparticus978@gmail.com>
2020-12-28 13:13:55 -0800

    styling added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -15,25 +15,40 @@ class Nav extends React.Component {
   render() {
     return (
       <nav className="nav">
-        <h1>
-          <Link to="/">BAP</Link>
-        </h1>
-
         {TokenService.hasAuthToken() ? (
           <div className="nav-main">
-            <Link to={`/${this.context.user_name}/patterns`}>Dashboard</Link>
-            <Link to="/" onClick={() => this.logout()}>
+            <Link
+              className="nav-link-button"
+              to={`/${this.context.user_name}/patterns`}
+            >
+              Dashboard
+            </Link>
+            <Link
+              className="nav-link-button"
+              to="/"
+              onClick={() => this.logout()}
+            >
               Logout
             </Link>
           </div>
         ) : (
           <div className="nav-main">
-            <Link to="/signup">Signup</Link>
-            <Link to="/login">Login</Link>
+            <Link className="nav-link-button" to="/signup">
+              Signup
+            </Link>
+            <Link className="nav-link-button" to="/login">
+              Login
+            </Link>
           </div>
         )}
-        <Link to="/sequencer">New</Link>
-        <div className="nav-authed"></div>
+        <h1>
+          <Link className="home-link" to="/">
+            BAP
+          </Link>
+        </h1>
+        <Link className="nav-link-button" to="/sequencer">
+          New Pattern
+        </Link>
       </nav>
     );
   }
--- src/routes/Landing.js
+++ src/routes/Landing.js
@@ -1,12 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./Landing.css";
 
 export default function Landing() {
   return (
-    <section className="Landing">
-      <p>Get started by clicking 'New'</p>
-      <p></p>
+    <section className="landing">
+      <h1>WELCOME TO BAP</h1>
+      <h3>Your very own beat machine!</h3>
+      <p>
+        This app is a drum sequencer. You are given a grid of buttons, each row
+        is a different sound and each column is 1 beat out of 16. When you
+        select a button from the grid and press 'Play', that sound will trigger
+        on the coresponding beat. If you are new to this, just press play and
+        start selecting boxes. It will make noise, and you will find your own
+        sound. Never give up!
+      </p>
+      <h3>Try it out! Click:</h3>
+      <Link className="nav-link-button" to="/sequencer">
+        New Pattern
+      </Link>
+      <h3>To save your patterns, create an account:</h3>
+      <Link className="nav-link-button" to="/signup">
+        Signup
+      </Link>
+      <h3>Or log in to an existing account:</h3>
+      <Link className="nav-link-button" to="/login">
+        Login
+      </Link>
     </section>
   );
 }
